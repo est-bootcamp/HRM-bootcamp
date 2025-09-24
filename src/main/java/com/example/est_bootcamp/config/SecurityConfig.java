7a13faaf962f5252c5b70e077fced1eb52a40096
jsw <wjstmddnjs5@daum.net>
2025-09-24 10:25:56 +0900

    AuthController, SecurityConfig 수정

--- src/main/java/com/example/est_bootcamp/config/SecurityConfig.java
+++ src/main/java/com/example/est_bootcamp/config/SecurityConfig.java
@@ -1,35 +1,51 @@
 package com.example.est_bootcamp.config;
 
-import com.example.est_bootcamp.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthFilter jwtAuthFilter;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll() // 로그인, 정적 리소스 허용
+                        .anyRequest().authenticated() // 나머지는 인증 필요
                 )
-                .addFilterBefore(jwtAuthFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
-                .httpBasic(Customizer.withDefaults());
+
+                .formLogin(form -> form
+                        .loginPage("/login")              // 로그인 페이지 (GET)
+                        .loginProcessingUrl("/login")     // 로그인 요청 처리 (POST)
+                        .defaultSuccessUrl("/main", true) // 로그인 성공 시 이동
+                        .failureUrl("/login?error=true")  // 실패 시 다시 로그인 페이지
+                        .permitAll()
+                )
+
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout=true")
+                );
+
         return http.build();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration cfg) throws Exception {
         return cfg.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
